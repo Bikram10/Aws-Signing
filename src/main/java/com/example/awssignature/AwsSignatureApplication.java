ada4a825cad3b253bab44c3436a1e4e068ee5946
Bikram <sbikrm@gmail.com>
2021-12-31 07:49:44 +0800

    "First Version"

---
+++ src/main/java/com/example/awssignature/AwsSignatureApplication.java
@@ -0,0 +1,13 @@
+package com.example.awssignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSignatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSignatureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awssignature/AwsSignatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssignature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSignatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
